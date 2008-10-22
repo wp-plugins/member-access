385f289c796229e1d6b82e8c6f32b54071dd64ae
Brownoxford <Brownoxford@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-22 04:04:11 +0000

    Updating version in 0.1 tag
    git-svn-id: https://plugins.svn.wordpress.org/member-access/tags/0.1@69874 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- member_access.php
+++ member_access.php
@@ -3,7 +3,7 @@
  * Plugin Name: Member Access
  * Plugin URI:  http://www.chrisabernethy.com/wordpress-plugins/member-access/
  * Description: Member Access is a WordPress plugin that allows an administrator to require that users be logged-in in order to view certain posts and pages.
- * Version:     0.2
+ * Version:     0.1
  * Author:      Chris Abernethy
  * Author URI:  http://www.chrisabernethy.com/
  * 
